16ad75b6506ec882e6fecd5089012968e75a21b0
iamprabin <meprabin000@gmail.com>
2022-11-29 20:35:37 -0600

    Added the correct implementation for robot

--- behavior_control/main.py
+++ behavior_control/main.py
@@ -26,11 +26,12 @@ TSR = TouchSensor(Port.S3)
 TSL = TouchSensor(Port.S4)
 
 # configurations
-slight_movement_speed = 100
-slight_turn_speed = 50
+slight_movement_speed = 200
+slight_turn_speed = 100
 
-move_time = 1000
-turn_time = 1000
+move_time = 500
+turn_time = 500
+sleep_time = 1
 
 
 
@@ -65,7 +66,7 @@ def goal_found():
     return CS.color() == Color.GREEN
 
 def extinguish():
-    pass
+    ev3.speaker.say("Danger averted")
 
 # detect if the wall is in the front
 def wall_front():
@@ -77,32 +78,32 @@ def wall_left():
 
 # moves the robot forward in the given speed
 def slight_forward():
-    time.sleep(move_time/1000+0.5)
+    time.sleep(sleep_time)
     LM.run_time(slight_movement_speed, move_time, Stop.COAST, False)
     RM.run_time(slight_movement_speed, move_time, Stop.COAST, False) 
 
 def slight_backward():
-    time.sleep(move_time/1000+0.5)
+    time.sleep(sleep_time)
     LM.run_time(-slight_movement_speed,move_time,Stop.HOLD,False)
     RM.run_time(-slight_movement_speed,move_time,Stop.HOLD,False)
 
 def slight_right():
-    time.sleep(turn_time/1000+0.5)
+    time.sleep(sleep_time)
     LM.run_time(slight_turn_speed, turn_time, Stop.COAST, False)
     RM.run_time(-slight_turn_speed, turn_time, Stop.COAST, False)    
 
 def slight_left():
-    time.sleep(turn_time/1000+0.5)
+    time.sleep(sleep_time)
     LM.run_time(-slight_turn_speed, turn_time, Stop.COAST, False)
     RM.run_time(slight_turn_speed, turn_time, Stop.COAST, False) 
 
 def very_slight_right():
-    time.sleep(turn_time/1000+0.5)
+    time.sleep(sleep_time)
     LM.run_time(slight_turn_speed, turn_time/2, Stop.COAST, False)
     RM.run_time(-slight_turn_speed, turn_time/2, Stop.COAST, False)    
 
 def very_slight_left():
-    time.sleep(turn_time/1000+0.5)
+    time.sleep(sleep_time)
     LM.run_time(-slight_turn_speed, turn_time/2, Stop.COAST, False)
     RM.run_time(slight_turn_speed, turn_time/2, Stop.COAST, False) 
 
@@ -122,13 +123,13 @@ def print_sensor_readings():
     log("Color Sensor: ", CS.color())
 
 # follow the wall
-def follow_wall():
-    wander_step = 10
-    current_step = 0
-    while True:
-        time.sleep(2)
+def follow_wall(steps):
+    ev3.speaker.say("Following the wall now!")
+    for i in range(steps):
+        time.sleep(0.5)
         if goal_found():
             extinguish()
+            return True
 
         elif TSL.pressed() and TSR.pressed():
             log("Full wall hit")
@@ -180,20 +181,53 @@ def follow_wall():
             else: # wander straight
                 for i in range(2): slight_forward()
 
-        current_step += 1
         print_sensor_readings()
+    return False
 
+def find_wall():
+    ev3.speaker.say("Finding the wall now!")
+    while(True):
+        if goal_found():
+            extinguish()
+            return True
+        elif TSR.pressed() or TSL.pressed():
+            return False
+        slight_forward()
 
-def test_sensor():
-    while True:
-        log("Color sensor: ", CS.color())
-        time.sleep(2)
+def wander(steps):
+    ev3.speaker.say("Wandering now!")
+    for i in range(steps):
+        if goal_found():
+            extinguish()
+            return True
+        elif TSR.pressed() or TSL.pressed():
+            for i in range(2): slight_backward()
+            if random() < 0.5: 
+                for i in range(5): slight_right()
+            else:
+                for i in range(5): slight_left()
+        elif random() < 0.5: 
+            for i in range(3): slight_left()
+        elif random() < 1: 
+            for i in range(3): slight_right()        
+        for i in range(3): slight_forward()
+    return False
 
+def run():
+    wall_following_step = 80
+    wander_step = 15
+    current_time_step = 0
+    goal_reached = False
+    find_wall()
+    while not goal_reached:
+            if not goal_reached: goal_reached = follow_wall(wall_following_step)
+            if not goal_reached: goal_reached = wander(wander_step)
+            if not goal_reached: goal_reached = find_wall()
+            
 
-        
+    for i in range(3): ev3.speaker.say("Exit the Building!")
 
-follow_wall()
-#test_sensor()
+run()
 
 # Write your program here.
 ev3.speaker.beep()
